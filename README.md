4ca0b33e6ddd7287401b19395950e9a858218e44
Rob Wells <rob@robjwells.com>
2025-07-17 01:13:13 +0100

    Startup pattern, comments, wiring information

--- README.md
+++ README.md
@@ -7,13 +7,25 @@ It uses the `INTERNAL` voltage (about 1.1V) as the analog reference, as the
 TMP36 only reaches 1V at about 50°C, and I'm using this as a basic room
 thermometer.
 
-The `INTERNAL` voltage on an Atmega 328P isn't exactly 1.1V, so you'll want to 
+The `INTERNAL` voltage on an Atmega 328P isn't exactly 1.1V, so you'll want to
 [measure the voltage on your particular chip][measure] and substitute in the
-value in millivolts.
+value in millivolts. You could use the default analog reference (5V), but then
+you'll only be able to measure in roughly 0.5°C increments.
 
 [measure]: https://forum.arduino.cc/t/measurement-of-bandgap-voltage/38215/4
 
-You could use the default analog reference (5V), but then you'll only be able
-to measure in roughly 0.5°C increments.
+### Breadboard wiring
 
-When I have time, I'll add a wiring diagram and some additional details.
+![Fritzing diagram that shows the breadboard wiring](./Temperature Display_bb.png)
+
+There are a lot of wires! Each 7-segment display takes 8 wires, for 24 wires total.
+Each display is wired up in the same way, with Q0 from the shift register wired to
+pin 1 (segment E) of the display.
+
+### Schematic
+
+![Circuit schematic.](./Temperature Display_schem.png)
+
+Note that the data is shifted out from the Metro Mini to U3, the tenths-place
+shift register, which is in turn chained to U2 (U3 Q7' to U2 data in), which is
+in turn chained to U1.
--- arduino-temperature-display.ino
+++ arduino-temperature-display.ino
@@ -1,17 +1,23 @@
+// Pins
 #define TEMP_INPUT A0
 #define SHIFT_DATA 2
 #define SHIFT_CLOCK 3
 #define STORE 4
 
-const int DELAY_SECONDS = 60;
+// Update period over which the temperature readings are averaged.
+// This helps avoid fluctuations in the displayed temperature.
+#define DELAY_SECONDS 60
 
-// Measured voltage (in mV) of the INTERNAL analog voltage reference.
+// Measured voltage (in mV) of the internal analog voltage reference.
+// The internal reference is used (rather than the 5V reference) to better
+// match the output voltage range of the TMP36 (0.1V to 1.75V).
 const float INTERNAL_VOLTAGE_MV = 1053.0;
+// Pre-compute the ADC step value.
+const float analogStep = INTERNAL_VOLTAGE_MV / 1023.0;
 
+// Active-low bit patterns for decimal numbers on the 7-segment displays.
+// Bit order: EDC.BAFG
 const byte segments[10] = {
-    // Bit order: EDC.BAFG
-    // Active low.
-
     // 0: ABCDEF
     0b00010001,
     // 1: BC
@@ -34,54 +40,91 @@ const byte segments[10] = {
     0b10010000,
 };
 
-byte setDecimalPoint(byte pattern) {
-    // Segments are active low.
-    byte pointOnly = 0b11101111;
-    return pattern & pointOnly;
+// Bit pattern to enable the decimal point.
+const byte decimalPoint = 0b11101111;
+
+// Pulse the shift registers' store clock to move the shifted-in data to the
+// registers' output stage.
+void pulseStoreClock() {
+    digitalWrite(STORE, HIGH);
+    delay(1);
+    digitalWrite(STORE, LOW);
 }
 
+// Displays a specific pattern on the 7-segment displays until a real
+// temperature reading is available.
+//
+// The shift register contents are unknown on power-up, so this avoids
+// weird-looking garbage that makes it look like there's been a fault.
+void writeResetPattern() {
+    // X-like pattern
+    const byte resetSequence[] = {
+        // Bit order: EDC.BAFG
+        // AG
+        0b10111011,
+        // G
+        0b11111110,
+        // AG
+        0b10111011,
+    };
+
+    for (int idx = 0; idx < 3; idx += 1) {
+        shiftOut(SHIFT_DATA, SHIFT_CLOCK, LSBFIRST, resetSequence[idx]);
+    }
+    pulseStoreClock();
+}
+
+// Read temperature from the TMP36.
 int readTemperatureInTenthsC() {
-  int raw = analogRead(TEMP_INPUT);
-  float mv = raw * (INTERNAL_VOLTAGE_MV / 1023.0);
-  float tenths = mv - 500;
-  return (int) tenths;
+    // milliVolt reading from the TMP36
+    float mv = analogRead(TEMP_INPUT) * analogStep;
+    // Remove the 500mV offset to produce the temperature value in 1/10 °C
+    float tenths = mv - 500;
+    return (int)tenths;
 }
 
-int buffer[DELAY_SECONDS];
+// Intermediate temperature readings (one per second of delay).
+int readingBuffer[DELAY_SECONDS];
+
+// Average the readings in readingBuffer.
+//
+// Returned value is in 10ths of a degree.
+int averageOfReadings() {
+    int average = 0;
+    for (int idx = 0; idx < DELAY_SECONDS; idx += 1) {
+        average += readingBuffer[idx];
+    }
+    return average / DELAY_SECONDS;
+}
 
 void setup() {
-  // Use the internal ~1.1 volt reference
-  // Actually 1.053v on my Metro Mini.
-  analogReference(INTERNAL);
-  pinMode(TEMP_INPUT, INPUT);
-  pinMode(SHIFT_DATA, OUTPUT);
-  pinMode(SHIFT_CLOCK, OUTPUT);
-  pinMode(STORE, OUTPUT);
-  digitalWrite(STORE, LOW);
+    analogReference(INTERNAL);      // Internal ~1.1V analog reference.
+
+    pinMode(TEMP_INPUT, INPUT);     // TMP36 analog input (0.1-2.0V).
+
+    pinMode(SHIFT_DATA, OUTPUT);    // 10ths place shift register data input.
+    pinMode(SHIFT_CLOCK, OUTPUT);   // Shift registers' clock input.
+    pinMode(STORE, OUTPUT);         // Store clock to move data to output stage.
+    digitalWrite(STORE, LOW);       // Ensure store clock is low.
+
+    writeResetPattern();            // Initialise the display to a known pattern.
 }
 
 void loop() {
-  for (int idx = 0; idx < DELAY_SECONDS; idx++) {
-    buffer[idx] = readTemperatureInTenthsC();
-    delay(1000);
-  }
+    for (int idx = 0; idx < DELAY_SECONDS; idx += 1) {
+        readingBuffer[idx] = readTemperatureInTenthsC();
+        delay(1000);
+    }
 
-  int average = 0;
-  for (int idx = 0; idx < DELAY_SECONDS; idx++) {
-    average += buffer[idx];
-  }
-  int tenthsC = average / DELAY_SECONDS;
-  byte tensPlace = tenthsC / 100;
-  byte onesPlace = tenthsC / 10 % 10;
-  byte tenthsPlace = tenthsC % 10;
-  
-  // Write out the digits in order left-right.
-  shiftOut(SHIFT_DATA, SHIFT_CLOCK, LSBFIRST, segments[tensPlace]);
-  shiftOut(SHIFT_DATA, SHIFT_CLOCK, LSBFIRST, setDecimalPoint(segments[onesPlace]));
-  shiftOut(SHIFT_DATA, SHIFT_CLOCK, LSBFIRST, segments[tenthsPlace]);
+    int tenthsC = averageOfReadings();
+    byte tensPlace = tenthsC / 100;
+    byte onesPlace = tenthsC / 10 % 10;
+    byte tenthsPlace = tenthsC % 10;
 
-  // Pulse the shift registers' store clock.
-  digitalWrite(STORE, HIGH);
-  delay(1);
-  digitalWrite(STORE, LOW);
+    // Write out the digits in order left-right.
+    shiftOut(SHIFT_DATA, SHIFT_CLOCK, LSBFIRST, segments[tensPlace]);
+    shiftOut(SHIFT_DATA, SHIFT_CLOCK, LSBFIRST, segments[onesPlace] & decimalPoint);
+    shiftOut(SHIFT_DATA, SHIFT_CLOCK, LSBFIRST, segments[tenthsPlace]);
+    // Move data into output stage to display.
+    pulseStoreClock();
 }
